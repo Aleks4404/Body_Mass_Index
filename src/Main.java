6c6e20d536356316432a59bdc583d316e0c96a7e
Aleksey Efimov <Aleks4404@mail.ru>
2021-04-09 05:24:23 +0400

    BmiService

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+
+        double bmi = service.calculate(70, 1.73);
+
+        System.out.println("Индекс массы тела:" + bmi);
+    }
+}
